90367294e6cc08856d4e32fe2dd21fda1ab63ef6
1511430 <1511430@2F16-30>
2017-02-22 14:13:08 -0500

    qwer

--- Pacman/PacManLibrary/Maze.cs
+++ Pacman/PacManLibrary/Maze.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -35,6 +36,64 @@ namespace PacManLibrary
         public int Size
         {
             get{ return maze.GetLength(0); }
+        }
+        public List<Tile> GetAvailableNieghbours(Vector2 position, Direction dir)
+        {
+            List<Tile> available_tiles = new List<Tile>();
+
+            switch (dir)
+            {
+                case Direction.Down:
+                    /*if (maze[(int)(position.Y + 1), position.X] != maze[1, 1])
+                    {
+                        available_tiles.Add(maze[1, 1]);
+                    }*/
+                    break;
+                case Direction.Up:
+                    /* if (maze[(int)(position.Y + 1), position.X] != maze[1, 1])
+                     {
+                         available_tiles.Add(maze[1, 1]);
+                     }*/
+                    break;
+                case Direction.Left:
+                    /* if (maze[(int)(position.Y + 1), position.X] != maze[1, 1])
+                     {
+                         available_tiles.Add(maze[1, 1]);
+                     } */
+                    break;
+                case Direction.Right:
+                    /* if (maze[(int)(position.Y + 1), position.X] != maze[1, 1])
+                     {
+                         available_tiles.Add(maze[1, 1]);
+                     } */
+                    break;
+
+            }
+            return available_tiles;
+        }
+
+        public void CheckMembersLeft()
+        {
+            bool chk = true;
+            for (int i = 0; i < maze.GetLength(0); i++)
+            {
+                for (int j = 0; j < maze.GetLength(0); j++)
+                {
+                    if (maze[i,j] != null)
+                    {
+                        chk = false;
+                    }
+                }
+            }
+            if (chk)
+            {
+                // won();
+                //won hhj = PacmanWon;
+               // PacmanWon += { 5; };
+                
+            }
+
+           
         }
         
     }

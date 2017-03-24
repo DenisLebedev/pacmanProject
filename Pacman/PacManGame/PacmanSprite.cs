6a32dc86d116a6addd56834f99850ecd568bdc2d
1511430 <1511430@2F16-32>
2017-03-24 15:04:12 -0400

    sad

--- Pacman/PacManGame/PacmanSprite.cs
+++ Pacman/PacManGame/PacmanSprite.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PacManLibrary;
 
 namespace PacManGame
 {
     class PacmanSprite
     {
+        private Game1 game1;
+        private GameState gs;
+
+        public PacmanSprite(Game1 game1, GameState gs)
+        {
+            this.game1 = game1;
+            this.gs = gs;
+        }
     }
 }

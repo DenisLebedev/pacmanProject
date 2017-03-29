c7620603f4cb882929b7e4e9193a25259331f179
Cookie <1532444@2F24-24>
2017-03-29 12:25:45 -0400

    Ghost cooment

--- Pacman/PacManGame/GhostSprite.cs
+++ Pacman/PacManGame/GhostSprite.cs
@@ -13,6 +13,11 @@ using Microsoft.Xna.Framework.Media;
 
 namespace PacManGame
 {
+    /// <summary>
+    /// GhostSprite is responsable to draw the ghost depending his state.
+    /// A scared ghost look different from a normal ghost.
+    /// Also, the class take care to load all the ghosts.
+    /// </summary>
     public class GhostSprite : DrawableGameComponent
     {
         //to render
@@ -22,28 +27,34 @@ namespace PacManGame
         private Game1 game;
         private GameState gs;
 
-        private int counter;
+        private int counterSpeed;
         private int speedLimit;
 
         private int frameGP;
         private int framecounter;
 
+        /// <summary>
+        /// Initialisation of all variables needed.
+        /// </summary>
+        /// <param name="game"> game is necessary to use special properties from this class.</param>
+        /// <param name="gs">gs help us to keep track of the game.</param>
         public GhostSprite(Game1 game, GameState gs) : base(game)
         {
             imgGhost = new List<Texture2D>(4);
             this.gs = gs;
             this.game = game;
-            counter = 0;
+
+            counterSpeed = 0;
+            //A ghost canot go faster than seedLimit which is the same as PacMan
             speedLimit = 10;
             framecounter = 1;
             frameGP = 1;
         }
 
-        public override void Initialize()
-        {
-            base.Initialize();
-        }
-
+        /// <summary>
+        /// The method does load all your texture and external needed
+        /// resources.
+        /// </summary>
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
@@ -57,20 +68,30 @@ namespace PacManGame
             base.LoadContent();
         }
 
+        /// <summary>
+        /// Update will update the screen view from a user persective
+        /// .
+        /// </summary>
+        /// <param name="gameTime"></param>
         public override void Update(GameTime gameTime)
         {
             
    
-            if (counter > speedLimit)
+            if (counterSpeed > speedLimit)
             {
                 gs.GhostPack.Move();
                 gs.GhostPack.CheckCollideGhosts(gs.Pacman.Position);
-                counter = 0;
+                counterSpeed = 0;
                 base.Update(gameTime);
             }
-            counter++;
+            counterSpeed++;
         }
 
+        /// <summary>
+        /// Draw will draw all the necessary items for a ghost and change
+        /// pictures when a ghost state is changed.
+        /// </summary>
+        /// <param name="gameTime"></param>
         public override void Draw(GameTime gameTime)
         {
             spriteBatch.Begin();
@@ -81,6 +102,7 @@ namespace PacManGame
             {
                 if (g.CurrenState == GhostState.Scared)
                 {
+                    //New picture for a scared ghost
                     spriteBatch.Draw(imgScaredG, new Rectangle((int)(g.Position.X) * 32,
                                            (int)(g.Position.Y) * 32, 32, 32), new Rectangle(32 * frameGP, 0, 32, 32)
                                               , Color.White);
@@ -88,13 +110,15 @@ namespace PacManGame
                 }
                 else
                 {
-                    spriteBatch.Draw(imgGhost.ElementAt(tempC), new Rectangle((int)(g.Position.X) * 32,
+                    //Using 4 different pictures for the ghost
+                    spriteBatch.Draw(imgGhost[tempC], new Rectangle((int)(g.Position.X) * 32,
                                            (int)(g.Position.Y) * 32, 32, 32), new Rectangle(32 * frameGP, 0, 32, 32), Color.White);
                     speedLimit = 10;
                 }
                 tempC++;
             }
 
+            //Updating the sprite of a ghost
             if (framecounter > 60)
             {
                 frameGP++;
